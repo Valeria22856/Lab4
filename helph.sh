90a3567692065eee0252ab26d888a64f819a618e
Valeriia22856 <dsw51765@student.dsw.edu.pl>
2024-05-27 09:36:10 +0200

    Modyfikacja

--- helph.sh
+++ helph.sh
@@ -5,5 +5,7 @@ if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     echo "--logs [n], -l [n]   Create log files (default: 100)"
     echo "--error [n], -e [n]   Create error files (default: 100)"
     echo "--help, -h       Display help"
+    echo "--init           Clone repository and set PATH"
 fi' > helph.sh
 chmod +x helph.sh
+
